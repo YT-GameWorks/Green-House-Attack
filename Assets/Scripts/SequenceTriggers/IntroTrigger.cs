1590b3ff640419bb8a2f89a868fefc02143448b9
GameWorks <sinha.soham@outlook.com>
2020-11-11 21:54:27 +0530

    Added intro sequence(Camera shake and switching only), added sfx, remove camera follow (to be added later), Refactoring and update gitignore.

---
+++ Assets/Scripts/SequenceTriggers/IntroTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace GreenHouseAttack.SequenceTriggers
+{
+	public class IntroTrigger : MonoBehaviour
+	{
+		public PlayableDirector IntroDirector;
+
+		void Start()
+		{
+			Invoke("PlayIntro", 1.5f);
+		}
+
+		private void PlayIntro()
+		{
+			IntroDirector.Play();
+		}
+	}
+}
